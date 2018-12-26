462c89717029550de05714ebec49e1f91615592f
Gal <gal.barak4@gmail.com>
2018-12-26 02:56:42 +0200

    Update careful_steps.py

--- Programming/Careful Steps/careful_steps.py
+++ Programming/Careful Steps/careful_steps.py
@@ -2,22 +2,22 @@ import rarfile
 import zipfile
 import os
 
-PATH = '/home/gal/Downloads/archives/'
+PATH = 'archives/'
 
 comments = []
 count = 0
 i = 0
-res = ''
+flag = ''
 
 while count < 120:
     try:
-        file = rarfile.RarFile('/home/gal/Downloads/archives/unzipme.{0}'.format(i), 'r')
+        file = rarfile.RarFile('archives/unzipme.{0}'.format(i), 'r')
         c, j = file.comment.strip().split(',')
     except rarfile.BadRarFile:
-        file = zipfile.ZipFile('/home/gal/Downloads/archives/unzipme.{0}'.format(i), 'r')
+        file = zipfile.ZipFile('archives/unzipme.{0}'.format(i), 'r')
         c, j = file.comment.decode().strip().split(',')
 
-    res += c
+    flag += c
     i += int(j)
     count += 1
-print(res)
+print(flag)
